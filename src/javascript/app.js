1ac55295dd3290e154a7d8c5ca498e9d11a0fa3f
Álex Prado <alexprado@gmail.com>
2017-04-20 18:23:35 -0300

    -Tela de Apresentação e Troca de telas

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,1 +1,28 @@
 //Codigo JS do APP
+let btn_comecar = document.getElementById('btn-comecar');
+let activity_list = document.querySelectorAll('.activity');
+
+btn_comecar.addEventListener('touchend', evt => {
+	remove_main();
+	mostra_tela('activity-principal');
+});
+
+let remove_main = () => {
+	for(let elem of activity_list)
+	{
+		if(elem.classList.contains('activity-main'))
+		{
+			elem.classList.remove('activity-main');
+		}
+	}
+}
+
+let mostra_tela = (id) => {
+	for(let elem of activity_list)
+	{
+		if(elem.id == id)
+		{
+			elem.classList.add('activity-main');
+		}
+	}
+}
